eac8badd0004e0e43a4427ca1a61e04372d640a2
salt7534TW <47550015+salt7534TW@users.noreply.github.com>
2021-08-09 20:16:15 +0900

    wetube middleware

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+
+const handleHome = (req, res) => {
+    return res.send("I love middleware");
+}
+
+app.get("/",logger, handleHome);
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+app.listen(PORT, handleListening);
