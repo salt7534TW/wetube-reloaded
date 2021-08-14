57f05a4b9f59e2cd3c59384f52af598524ab75a7
salt7534TW <47550015+salt7534TW@users.noreply.github.com>
2021-08-15 00:55:39 +0900

    8/15

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+
 
 const PORT = 4000;
 
@@ -12,6 +14,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended : true}));
 app.use("/", globalRouter);
 app.use("/users", userRouter); 
 app.use("/videos", videoRouter);
