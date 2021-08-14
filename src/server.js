a21bf20839deb1e8ca0d208c57300818b3444f12
salt7534TW <47550015+salt7534TW@users.noreply.github.com>
2021-08-14 16:35:45 +0900

    8/13

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
+
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use("/", globalRouter);
+app.use("/users", userRouter); 
+app.use("/videos", videoRouter);
 
-
-const handleHome = (req, res) => {
-    return res.send("I love middleware");
-}
-
-app.get("/",logger, handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
