1835493eadc5004f69c2cb94c008c75a284a9f4c
salt7534TW <47550015+salt7534TW@users.noreply.github.com>
2021-08-31 01:23:46 +0900

    8/30

---
+++ src/routers/rootRouter.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/userController";
+import { home, search } from "../controllers/videoController";
+
+const rootRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+const handleJoin = (req, res) => res.send("Join");
+
+rootRouter.get("/", home);
+rootRouter.route("/join").get(getJoin).post(postJoin);
+rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.get("/search", search);
+
+export default rootRouter;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
-
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import { localsMiddleware } from "./middlewares";
+import { MongoServerSelectionError } from "mongodb";
 
 const app = express();
 const logger = morgan("dev");
@@ -13,11 +14,20 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use(express.urlencoded({ extended : true}));
-app.use("/", globalRouter);
-app.use("/users", userRouter); 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
+app.use(localsMiddleware);
+app.use("/", rootRouter);
+app.use("/users", userRouter);
 app.use("/videos", videoRouter);
-
-
 
 export default app;
